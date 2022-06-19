85f5a63f6580f38483d0bf4f996942b0b6133ff4
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2022-06-19 22:30:26 +0400

    unit of work

---
+++ BillingAPI/Data/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using BillingAPI.Data.interfaces;
+
+namespace BillingAPI.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _dataContext;
+
+        public UnitOfWork(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public IBalanceRepository BalanceRepository => new BalanceRepository(_dataContext);
+
+        public IGatewayRepository GatewayRepository => new GatewayRepository(_dataContext);
+
+        public IOrderRepository OrderRepository => new OrderRepository(_dataContext);
+
+        public IPaymentRepository PaymentRepository => new PaymentRepository(_dataContext);
+
+        public IUserRepository UserRepository => new UserRepository(_dataContext);
+
+        public bool HasChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> SaveChanges()
+        {
+            return await _dataContext.SaveChangesAsync() > 0;
+        }
+    }
+}
--- BillingAPI/Extensions/ApplicationServiceExtensions.cs
+++ BillingAPI/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using BillingAPI.Data;
+using BillingAPI.Data.interfaces;
 using BillingAPI.Helpers;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,7 @@ namespace BillingAPI.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
