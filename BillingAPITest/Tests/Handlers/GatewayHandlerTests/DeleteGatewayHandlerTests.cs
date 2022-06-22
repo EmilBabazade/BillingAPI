80249c932bf3b4dd445597a652d1e9a707909140
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2022-06-22 22:38:27 +0400

    Deletegateway and its tests

---
+++ BillingAPITest/Tests/Handlers/GatewayHandlerTests/DeleteGatewayHandlerTests.cs
@@ -0,0 +1,63 @@
+﻿using BillingAPI.Entities;
+using BillingAPI.Mediatr;
+using BillingAPI.Mediatr.Handlers.GatewayHandlers;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BillingAPITest.Tests.Handlers.GatewayHandlerTests
+{
+    public class DeleteGatewayHandlerTests : BaseHandlerTests
+    {
+        private DeleteGatewayHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _handler = new DeleteGatewayHandler(_dataContext);
+        }
+
+        //// delete by id should delete
+        [Test]
+        public async Task DeleteByIdShouldLessenLengthBy1AndDelete()
+        {
+            // Arrange
+            int deleteId = _gateways[0].Id;
+
+            // Act
+            await _handler.Handle(new DeleteGatewayCommand(deleteId), new CancellationToken());
+            Gateway? deletedGateway = await _dataContext.Gateways.FindAsync(deleteId);
+            List<Gateway>? gatewaysInDB = await _dataContext.Gateways.ToListAsync();
+
+            // Assert
+            deletedGateway.Should().BeNull();
+            gatewaysInDB.Count.Should().Be(_gateways.Count - 1);
+        }
+
+        // delete by id should throw exception
+        [Test]
+        public async Task DeleteByIdShouldThrowNotFoundExceptionForNonExistentGateway()
+        {
+            // Arrange
+            int deleteId = -99999;
+            string? type = "";
+
+            // Act
+            try
+            {
+                await _handler.Handle(new DeleteGatewayCommand(deleteId), new CancellationToken());
+            }
+            catch (Exception ex)
+            {
+                type = ex.GetType()?.FullName;
+            }
+
+            // Assert
+            type.Should().Be("BillingAPI.Errors.NotFoundException");
+        }
+    }
+}
